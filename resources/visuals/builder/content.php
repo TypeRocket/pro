e4a0688efacfe584ad61a855190c0f595c38efac
Kevin Dees <kvdees@gmail.com>
2020-02-13 09:54:21 -0500

    add comment to visual

--- resources/visuals/builder/content.php
+++ resources/visuals/builder/content.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * Content - Visual
+ *
+ * @var array $data component fields
+ * @var bool $frist_item is first component
+ * @var bool $last_item is last component
+ * @var mixed|\TypeRocket\Models\Model $model model used by the component
+ */
+?>
 <div class="builder-content">
     <h2><?php echo esc_html($data['headline']); ?></h2>
     <?php echo wp_get_attachment_image($data['featured_image']); ?>
