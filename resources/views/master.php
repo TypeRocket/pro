a2f71b99422d1ecd3e957d30256fdf990b9c92f1
Kevin Dees <kvdees@gmail.com>
2020-03-06 08:07:17 -0500

    update pro splash screen

--- resources/views/master.php
+++ resources/views/master.php
@@ -5,17 +5,50 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/typerocket-public/galaxy/galaxy.css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500&display=swap"
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="//s3-us-west-2.amazonaws.com/typerocket-public/pro/pro.css">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class('light'); ?>>
 <?php wp_body_open(); ?>
 
 <section>
-    <img src="https://s3-us-west-2.amazonaws.com/typerocket-public/galaxy/typerocket-logo.svg" alt="TypeRocket Galaxy">
+    <img id="typerocket-logo" src="//s3-us-west-2.amazonaws.com/typerocket-public/pro/white-logo.svg" alt="TypeRocket Pro">
     <p><?php echo esc_html($title); ?></p>
 </section>
+
+<script>
+    function toggleTheme() {
+        document.body.classList.toggle("light");
+        document.body.classList.toggle("dark");
+
+        let theme = document.body.classList.contains("dark") ? "dark" : "light";
+
+        if(theme === 'dark') {
+            document.querySelector('#typerocket-logo').setAttribute('src', '//s3-us-west-2.amazonaws.com/typerocket-public/pro/logo.svg');
+        } else {
+            document.querySelector('#typerocket-logo').setAttribute('src', '//s3-us-west-2.amazonaws.com/typerocket-public/pro/white-logo.svg');
+        }
+
+        localStorage.setItem(
+            "colorTheme",
+            theme
+        );
+    }
+
+    if (localStorage.getItem("colorTheme") === "dark") {
+        toggleTheme();
+    } else if (
+        localStorage.getItem("colorTheme") !== "light" &&
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        toggleTheme();
+    }
+</script>
 
 <?php wp_footer(); ?>
 </body>
